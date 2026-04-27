a6a08a602347240717a0fbd26c27246d5d35f1e3
Paul <paulodinghin@gmail.com>
2026-04-27 09:53:35 +0200

    boost cmake

--- cmake/modules/FindOrFetchBoost.cmake
+++ cmake/modules/FindOrFetchBoost.cmake
@@ -1,70 +1,71 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # update the Boost version that we can tolerate
-if (NOT DEFINED Boost_OLDEST_BOOST_VERSION)
-    set(Boost_OLDEST_BOOST_VERSION ${TTG_OLDEST_BOOST_VERSION})
-else()
-    if (${Boost_OLDEST_BOOST_VERSION} VERSION_LESS ${TTG_OLDEST_BOOST_VERSION})
-        if (DEFINED CACHE{Boost_OLDEST_BOOST_VERSION})
-            set(Boost_OLDEST_BOOST_VERSION "${TTG_OLDEST_BOOST_VERSION}" CACHE STRING "Oldest Boost version to use" FORCE)
-        else()
-            set(Boost_OLDEST_BOOST_VERSION ${TTG_OLDEST_BOOST_VERSION})
+if(USE_PARSEC)
+    if (NOT DEFINED Boost_OLDEST_BOOST_VERSION)
+        set(Boost_OLDEST_BOOST_VERSION ${TTG_OLDEST_BOOST_VERSION})
+    else()
+        if (${Boost_OLDEST_BOOST_VERSION} VERSION_LESS ${TTG_OLDEST_BOOST_VERSION})
+            if (DEFINED CACHE{Boost_OLDEST_BOOST_VERSION})
+                set(Boost_OLDEST_BOOST_VERSION "${TTG_OLDEST_BOOST_VERSION}" CACHE STRING "Oldest Boost version to use" FORCE)
+            else()
+                set(Boost_OLDEST_BOOST_VERSION ${TTG_OLDEST_BOOST_VERSION})
+            endif()
         endif()
     endif()
-endif()
 
-# Boost can be discovered by every (sub)package but only the top package can *build* it ...
-# in either case must declare the components used by TTG
-set(required_components
-        headers
-        callable_traits
-)
-set(optional_components
-)
-if (TTG_PARSEC_USE_BOOST_SERIALIZATION)
-    list(APPEND optional_components
-            serialization
-            iostreams
+    # Boost can be discovered by every (sub)package but only the top package can *build* it ...
+    # in either case must declare the components used by TTG
+    set(required_components
+            headers
+            callable_traits
     )
-endif()
-if (BUILD_EXAMPLES)
-    list(APPEND optional_components
-            bimap   # dependency of graph
-            foreach # dependency of graph
-            graph   # used for generation of inputs for spmm
-            property_map # dependency of graph
-            xpressive # dependency of graph
+    set(optional_components
     )
-endif()
+    if (TTG_PARSEC_USE_BOOST_SERIALIZATION)
+        list(APPEND optional_components
+                serialization
+                iostreams
+        )
+    endif()
+    if (BUILD_EXAMPLES)
+        list(APPEND optional_components
+                bimap   # dependency of graph
+                foreach # dependency of graph
+                graph   # used for generation of inputs for spmm
+                property_map # dependency of graph
+                xpressive # dependency of graph
+        )
+    endif()
 
-# if not allowed to fetch Boost make all Boost optional
-if (NOT DEFINED Boost_FETCH_IF_MISSING AND TTG_FETCH_BOOST)
-    set(Boost_FETCH_IF_MISSING 1)
-endif()
-if (NOT Boost_FETCH_IF_MISSING)
-    foreach(__component IN LISTS required_components)
-    list(APPEND optional_components
-            ${__component}
-    )
-    endforeach()
-    set(required_components )
-endif()
+    # if not allowed to fetch Boost make all Boost optional
+    if (NOT DEFINED Boost_FETCH_IF_MISSING AND TTG_FETCH_BOOST)
+        set(Boost_FETCH_IF_MISSING 1)
+    endif()
+    if (NOT Boost_FETCH_IF_MISSING)
+        foreach(__component IN LISTS required_components)
+        list(APPEND optional_components
+                ${__component}
+        )
+        endforeach()
+        set(required_components )
+    endif()
 
-if (DEFINED Boost_REQUIRED_COMPONENTS)
-    list(APPEND Boost_REQUIRED_COMPONENTS
-            ${required_components})
-    list(REMOVE_DUPLICATES Boost_REQUIRED_COMPONENTS)
-else()
-    set(Boost_REQUIRED_COMPONENTS "${required_components}" CACHE STRING "Components of Boost to discovered or built")
+    if (DEFINED Boost_REQUIRED_COMPONENTS)
+        list(APPEND Boost_REQUIRED_COMPONENTS
+                ${required_components})
+        list(REMOVE_DUPLICATES Boost_REQUIRED_COMPONENTS)
+    else()
+        set(Boost_REQUIRED_COMPONENTS "${required_components}" CACHE STRING "Components of Boost to discovered or built")
+    endif()
+    if (DEFINED Boost_OPTIONAL_COMPONENTS)
+        list(APPEND Boost_OPTIONAL_COMPONENTS
+                ${optional_components}
+        )
+        list(REMOVE_DUPLICATES Boost_OPTIONAL_COMPONENTS)
+    else()
+        set(Boost_OPTIONAL_COMPONENTS "${optional_components}" CACHE STRING "Optional components of Boost to discovered or built")
+    endif()
 endif()
-if (DEFINED Boost_OPTIONAL_COMPONENTS)
-    list(APPEND Boost_OPTIONAL_COMPONENTS
-            ${optional_components}
-    )
-    list(REMOVE_DUPLICATES Boost_OPTIONAL_COMPONENTS)
-else()
-    set(Boost_OPTIONAL_COMPONENTS "${optional_components}" CACHE STRING "Optional components of Boost to discovered or built")
-endif()
-
 # Bring ValeevGroup cmake toolkit, if not yet available
 if(USE_PARSEC)
     include(FetchVGCMakeKit)
@@ -100,8 +101,6 @@ else()
             
             set(_boost_libs_root "${boost_SOURCE_DIR}/libs")
             
-            # On utilise une fonction de recherche pour ne prendre QUE les dossiers 'include' 
-            # directs des bibliothèques, ce qui est moins lourd qu'un GLOB récursif profond.
             file(GLOB _boost_includes RELATIVE ${boost_SOURCE_DIR} "${boost_SOURCE_DIR}/libs/*/include")
             
             foreach(_inc IN LISTS _boost_includes)
