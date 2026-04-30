90b8c3d94fa6573b5fc9715c0e06ae9d1f78d80a
Paul <paulodinghin@gmail.com>
2026-04-30 15:53:02 +0200

    boost hashmap

--- ttg/ttg/starpu/starpu_hash_table.h
+++ ttg/ttg/starpu/starpu_hash_table.h
@@ -12,7 +12,7 @@ typedef uintptr_t starpu_key_t;
 namespace ttg_starpu {
 
 
-    template <typename TT>
+    template <typename TT, typename KeyT >
     class starpu_hash_table : public boost::unordered::concurrent_flat_map<starpu_key_t, detail::starpu_ttg_task_t<TT>*> {
         private:
         using task_t = detail::starpu_ttg_task_t<TT>;
@@ -24,7 +24,7 @@ namespace ttg_starpu {
 
         //Visit an item if the key is present and apply func to it.
         //return true if the key is present, false otherwise.
-        template <typename KeyT, typename F>
+        template <typename F>
         bool starpu_hash_table_visit(KeyT key, F func){
             return this->visit(key, [&](typename flat_map_t::value_type &item){
                 func(item.second);
@@ -33,7 +33,7 @@ namespace ttg_starpu {
         }
         
         //Emplace an item if the key is not present, otherwise visit the item and apply func to it.
-        template <typename KeyT, typename F>
+        template <typename F>
         bool starpu_hash_table_emplace_or_visit(KeyT key, F &&func, task_t*&& new_task){
             return this->emplace_or_visit(key, new_task, [&](auto& item){
                 func(item.second);
@@ -42,7 +42,7 @@ namespace ttg_starpu {
         }
 
         //Emplace an item if the key is not present and apply F1 to it, otherwise visit the item and apply F2 to it.
-        template <typename KeyT, typename F1, typename F2>
+        template <typename F1, typename F2>
         bool starpu_hash_table_try_emplace_and_visit(KeyT key, F1 &&func_new, F2 &&func_visit){
             return this->try_emplace_and_visit(key, [&](){ return func_new(key); }, [&](auto& item){ func_visit(item.second); });
         }
@@ -50,7 +50,7 @@ namespace ttg_starpu {
         // Remove an item if the key is present and func returns true on it.
         // Returns deleted item pointer.
         //TODO : Need to delete task later
-        template <typename KeyT, typename F, typename... Args>
+        template <typename F, typename... Args>
         task_t* starpu_hash_table_remove(KeyT key,F &&func, Args&&... args){
             task_t* task = nullptr;
             this->erase_if(key, [&](auto& item){
