5a77f2b7c09453a7fe8464497da570dddc759df5
Paul <paulodinghin@gmail.com>
2026-06-02 14:52:16 +0200

    debuggin release_task

--- ttg/ttg/starpu/task.h
+++ ttg/ttg/starpu/task.h
@@ -151,16 +151,14 @@ namespace ttg_starpu {
       /* Protected constructors: this class should not be instantiated directly
        * but always be used through starpu_ttg_task_t. */
 
-      starpu_ttg_task_base_t(int data_count, ttg_data_copy_t **copies, release_task_fn *release_fn,
+      starpu_ttg_task_base_t(int data_count, ttg_data_copy_t **copies,
                             bool defer_writer_flag = true)
         : data_count(data_count)
         , copies(copies)
-	, release_task_cb(release_fn)
         , defer_writer(defer_writer_flag)
       {
         starpu_task = starpu_task_create();
         init_codelet_arg();
-	std::cerr << static_cast<void*>(this) << "->" << static_cast<void*>(starpu_task) << std::endl;
       }
 
       starpu_ttg_task_base_t(int32_t priority, int data_count, 
@@ -174,8 +172,6 @@ namespace ttg_starpu {
       {
         starpu_task = starpu_task_create();
         init_codelet_arg();
-std::cerr << static_cast<void*>(this) << "->" << static_cast<void*>(starpu_task) << std::endl;
-
       }
 
     public:
@@ -201,7 +197,7 @@ std::cerr << static_cast<void*>(this) << "->" << static_cast<void*>(starpu_task)
 
       
       starpu_ttg_task_t( TT *tt_ptr)
-        : starpu_ttg_task_base_t(num_streams, copies,&release_task)
+        : starpu_ttg_task_base_t(num_streams, copies)
         , tt(tt_ptr)
       {
         this->dev_ptr = this->dev_state.dev_ptr();
@@ -255,7 +251,7 @@ std::cerr << static_cast<void*>(this) << "->" << static_cast<void*>(starpu_task)
       starpu_ttg_task_t() = default;
 
       starpu_ttg_task_t( TT *tt_ptr)
-        : starpu_ttg_task_base_t(num_streams, copies,&release_task)
+        : starpu_ttg_task_base_t(num_streams, copies)
         , tt(tt_ptr)
       {
         this->dev_ptr = this->dev_state.dev_ptr();
